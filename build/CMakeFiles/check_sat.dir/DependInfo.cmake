159ef4b3d4b9f8dcd36cab9a5adddc1bf9a59992
YoshikiTakashima <y1takash@ucsd.edu>
2019-06-17 14:28:57 -0700

    Initialize Xcode stuff.

---
+++ build/CMakeFiles/check_sat.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/yoshitakashima/Desktop/dreal-cmake-example-project/check_sat.cc" "/Users/yoshitakashima/Desktop/dreal-cmake-example-project/build/CMakeFiles/check_sat.dir/check_sat.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/opt/ibex@2.7.4/include"
+  "/usr/local/opt/ibex@2.7.4/include/ibex"
+  "/usr/local/opt/ibex@2.7.4/include/ibex/3rd"
+  "/usr/local/opt/clp@1.17/include/clp/coin"
+  "/usr/local/opt/dreal/include"
+  "/usr/local/Cellar/nlopt/2.6.1/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/check_sat.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/check_sat.dir/check_sat.cc.o"
+  "check_sat.pdb"
+  "check_sat"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/check_sat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
